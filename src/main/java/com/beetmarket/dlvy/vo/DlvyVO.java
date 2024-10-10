f4d6dcbe0507374068b0d1db9c4761e0162f2790
Jhewon <119647139+Jhewon@users.noreply.github.com>
2024-10-10 21:27:15 +0900

    Delete src directory

--- src/main/java/com/beetmarket/dlvy/vo/DlvyVO.java
+++
@@ -1,9 +0,0 @@
-package com.beetmarket.dlvy.vo;
-
-import lombok.Data;
-
-@Data
-public class DlvyVO {
-	private long dlvyAddrNo,postNo,basic;
-	private String dlvyName,id,recipient,tel,addr,addrDetail;
-}
--- src/main/java/com/beetmarket/faq/vo/FaqCateVO.java
+++
@@ -1,9 +0,0 @@
-package com.beetmarket.faq.vo;
-
-import lombok.Data;
-
-@Data
-public class FaqCateVO {
-	private Long cateno;
-	private String catename;
-}
